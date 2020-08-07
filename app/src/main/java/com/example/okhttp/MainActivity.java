95f355456f4b24035ad41167d5b9cecd1b0c0ce8
cody.chen <cody.chen@itnsl.net>
2020-08-07 09:21:12 +0800

    dev2

--- app/src/main/java/com/example/okhttp/MainActivity.java
+++ app/src/main/java/com/example/okhttp/MainActivity.java
@@ -22,7 +22,7 @@ import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
-    // 建立OkHttpClient1
+    // 建立OkHttpClient2
     OkHttpClient client = new OkHttpClient().newBuilder().build();
 
     @Override
